75fd48a24e1fd598c2a9e94b34dc3d61dd88e887
Karel.Lannoy <karel@resengo.com>
2020-12-01 11:49:33 +0100

    Day01

---
+++ AdventOfCode2020/InputParser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020
+{
+    public static class InputParser
+    {
+        public static List<string> GetInputLines(string fileName)
+        {
+            string line;
+            var result = new List<string>();
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\..\..\inputs\" + fileName);
+            while ((line = file.ReadLine()) != null)
+            {
+                result.Add(line);
+            }
+
+            file.Close();
+            // Suspend the screen.  
+            return result;
+        }
+
+        public static string GetInputText(string fileName)
+        {
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\..\..\inputs\" + fileName);
+            var result = file.ReadToEnd();
+            // Suspend the screen.  
+            return result;
+        }
+
+        public static List<T> GetInputCommaSeperated<T>(string fileName)
+        {
+            var result = new List<T>();
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\..\..\inputs\" + fileName);
+            var text = file.ReadToEnd();
+
+            file.Close();
+
+            result = text.Split(",", StringSplitOptions.None).Select(i => (T)Convert.ChangeType(i, typeof(T))).ToList();
+            // Suspend the screen.  
+            return result;
+        }
+
+        public static List<List<string>> GetInputCommaSeperatedStringByLine(string fileName)
+        {
+            string line;
+            var result = new List<List<string>>();
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file =
+                new System.IO.StreamReader(@"..\..\..\inputs\" + fileName);
+
+            while ((line = file.ReadLine()) != null)
+            {
+                result.Add(line.Split(",", StringSplitOptions.None).ToList());
+            }
+
+            file.Close();
+
+            // Suspend the screen.  
+            return result;
+        }
+    }
+}
--- AdventOfCode2020/Program.cs
+++ AdventOfCode2020/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCode2020
 {
@@ -6,7 +7,69 @@ namespace AdventOfCode2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Day01_1());
+            Console.WriteLine(Day01_2());
+            Console.ReadLine();
+        }
+
+        private static int Day01_1()
+        {
+            var input = InputParser.GetInputLines("day01.txt");
+            var intList = new List<int>();
+            input.ForEach(inputItem => intList.Add(int.Parse(inputItem)));
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                var item1 = intList[i];
+                for (int y = 0; y < intList.Count; y++)
+                {
+                    if (i == y)
+                    {
+                        continue;
+                    }
+                    var item2 = intList[y];
+                    if (item1 + item2 == 2020)
+                    {
+                        return item1 * item2;
+                    }
+                }
+            }
+
+            throw new Exception("No Solution");
+        }
+
+        private static int Day01_2()
+        {
+            var input = InputParser.GetInputLines("day01.txt");
+            var intList = new List<int>();
+            input.ForEach(inputItem => intList.Add(int.Parse(inputItem)));
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                var item1 = intList[i];
+                for (int y = 0; y < intList.Count; y++)
+                {
+                    if (i == y)
+                    {
+                        continue;
+                    }
+                    var item2 = intList[y];
+                    for (int z = 0; z < intList.Count; z++)
+                    {
+                        if (i == z || y == z)
+                        {
+                            continue;
+                        }
+                        var item3 = intList[z];
+                        if (item1 + item2 + item3 == 2020)
+                        {
+                            return item1 * item2 * item3;
+                        }
+                    }
+                }
+            }
+
+            throw new Exception("No Solution");
         }
     }
 }
