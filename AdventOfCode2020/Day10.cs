7f3c69a31ed68fdca19161a26e317e727afdf331
Karel.Lannoy <karel@resengo.com>
2020-12-10 12:08:28 +0100

    Add comments and better variable names for future reference

--- AdventOfCode2020/Day10.cs
+++ AdventOfCode2020/Day10.cs
@@ -43,27 +43,31 @@ namespace AdventOfCode2020
             adapters.Insert(0, 0); // insert outlet
             adapters.Add(adapters.Last() + 3); //insert your device
 
-            var countDictionary = new Dictionary<int, long>();
+            var memory = new Dictionary<int, long>();
 
-            countDictionary[adapters.Count - 1] = 0; //your device is the endpoint
-            countDictionary[adapters.Count - 2] = 1; //your device is 3 away from the final step, so only 1 possible combination
+            memory[adapters.Count - 1] = 0; //your device is the endpoint
+            memory[adapters.Count - 2] = 1; //your device is 3 away from the final step, so only 1 possible combination
 
-            FindValidCombination(0, adapters, countDictionary);
-            return countDictionary[0];
+            FindValidCombination(0, adapters, memory);
+            return memory[0];
         }
 
-        private static long FindValidCombination(int index, List<int> adapters, Dictionary<int, long> countDictionary)
+        private static long FindValidCombination(int index, List<int> adapters, Dictionary<int, long> memory)
         {
-            if (countDictionary.ContainsKey(index)) return countDictionary[index];
+            // if possible steps to en for adapter in chain is already calculated, get it from memory
+            if (memory.ContainsKey(index)) return memory[index];
             long count = 0;
+            // next potential adapter can only be three places away 
+            // calculate number of potential steps to the end for those three places if the step is possible (<= 3)
             for (int i = 1; i <= 3; i++)
             {
                 if ((index + i < adapters.Count) && (adapters[index + i] - adapters[index] <= 3))
                 {
-                    count += FindValidCombination(index + i, adapters, countDictionary);
+                    count += FindValidCombination(index + i, adapters, memory);
                 }
             }
-            countDictionary[index] = count;
+            // Add calculated adapter possible steps to end to memory 
+            memory[index] = count;
             return count;
         }
     }
